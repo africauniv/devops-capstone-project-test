6eb64a7975de2dc37b0cf5a3e7e0bc32d9819e1b
africauniv <apollos.gamtchessi@africauniv.tech>
2024-05-17 03:20:14 -0400

    added build task

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -131,26 +131,25 @@ class TestAccountService(TestCase):
     ######################################################################
     #  A C C O U N T   T E S T   C A S E S S U P P L E M E N T S
     ######################################################################
-
     def test_read_account(self):
         """it should return an element by its id """
         ac = AccountFactory()
         ac.create()
         ac_result = Account.find_by_name(ac.name)[0]
         response = self.client.get(f"{BASE_URL}/{ac_result.id}")
-        self.assertEqual(response.status_code,status.HTTP_200_OK)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
         ac_final = response.get_json()
-        self.assertEqual(ac_final["name"],ac.serialize()["name"])
-        self.assertEqual(ac_final["email"],ac.serialize()["email"])
-        self.assertEqual(ac_final["address"],ac.serialize()["address"])
-        self.assertEqual(ac_final["phone_number"],ac.serialize()["phone_number"])
-    
+        self.assertEqual(ac_final["name"], ac.serialize()["name"])
+        self.assertEqual(ac_final["email"], ac.serialize()["email"])
+        self.assertEqual(ac_final["address"], ac.serialize()["address"])
+        self.assertEqual(ac_final["phone_number"], ac.serialize()["phone_number"])
+
     def test_read_not_found(self):
         """it should return an error not found type"""
         response = self.client.get(f"{BASE_URL}/20")
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         ac_final = response.get_json()
-        self.assertEqual("account not found",ac_final["error"])
+        self.assertEqual("account not found", ac_final["error"])
 
     def test_read_list_accounts(self):
         """it should return accounts list"""
@@ -170,9 +169,9 @@ class TestAccountService(TestCase):
     def test_read_list_not_found(self):
         """it should return an error not found type for list check"""
         response = self.client.get(BASE_URL)
-        self.assertEqual(response.status_code,status.HTTP_404_NOT_FOUND)
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
         ac_final = response.get_json()
-        self.assertEqual("accounts not found",ac_final["error"])
+        self.assertEqual("accounts not found", ac_final["error"])
 
     def test_update_account(self):
         """it should change value and return 200_OK"""
@@ -183,25 +182,25 @@ class TestAccountService(TestCase):
         ac.email = "benjamin@gmail.com"
         response = self.client.put(f"{BASE_URL}/{ac_id}", json=ac.serialize())
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        #check the item from database and verify modifications
+        # check the item from database and verify modifications
         ac_result = Account.find(ac_id)
         self.assertEqual("benjamin", ac_result.name)
         self.assertEqual("benjamin@gmail.com", ac_result.email)
 
     def test_update_fail(self):
         """it should try to change empty or unauthaurized attribut value Or either invalid id"""
-        response = self.client.put(f"{BASE_URL}/0", json = {})
+        response = self.client.put(f"{BASE_URL}/0", json={})
         self.assertEqual(response.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
 
         ac = AccountFactory()
-        response = self.client.put(f"{BASE_URL}/22", json = json.dumps(ac.serialize()))
+        response = self.client.put(f"{BASE_URL}/22", json=json.dumps(ac.serialize()))
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
         ac = AccountFactory()
         ac.create()
         ac_id = Account.find_by_name(ac.name)[0].id
         json_fail = '{"city":"goda"}'
-        response = self.client.put(f"{BASE_URL}/{ac_id}", json = json_fail)
+        response = self.client.put(f"{BASE_URL}/{ac_id}", json=json_fail)
         self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
 
     def test_delete_account(self):
@@ -211,23 +210,23 @@ class TestAccountService(TestCase):
         ac_id = Account.find_by_name(ac.name)[0].id
         response = self.client.delete(f"{BASE_URL}/{ac_id}")
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
-        #check the item from database and verify empty result
+        # check the item from database and verify empty result
         self.assertIsNone(Account.find(ac_id))
-    
+
     def test_delete_fail(self):
         """it should try to delete by false id"""
         response = self.client.delete(f"{BASE_URL}/22")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
 
     def test_check_https_security(self):
-        response = self.client.get(BASE_URL,environ_overrides=HTTPS_ENVIRON)
+        response = self.client.get(BASE_URL, environ_overrides=HTTPS_ENVIRON)
         result = response.headers
-        self.assertEqual('SAMEORIGIN',result["X-Frame-Options"])
-        self.assertEqual('nosniff',result["X-Content-Type-Options"])
-        self.assertEqual('default-src \'self\'; object-src \'none\'',result["Content-Security-Policy"])
-        self.assertEqual('strict-origin-when-cross-origin',result["Referrer-Policy"])
+        self.assertEqual('SAMEORIGIN', result["X-Frame-Options"])
+        self.assertEqual('nosniff', result["X-Content-Type-Options"])
+        self.assertEqual('default-src \'self\'; object-src \'none\'', result["Content-Security-Policy"])
+        self.assertEqual('strict-origin-when-cross-origin', result["Referrer-Policy"])
 
     def test_check_allow_route_protection(self):
-        response = self.client.get(BASE_URL,environ_overrides=HTTPS_ENVIRON)
+        response = self.client.get(BASE_URL, environ_overrides=HTTPS_ENVIRON)
         result = response.headers
-        self.assertEqual('*',result["Access-Control-Allow-Origin"])
+        self.assertEqual('*', result["Access-Control-Allow-Origin"])
